c5c9a8ab79414eec169b52ebee0c419bb2e8c6ca
ewdna <werdna1222@gmail.com>
2017-09-10 01:59:40 +0800

    sync

--- temp/ston/RID.js
+++ temp/ston/RID.js
@@ -3,8 +3,9 @@
 		$(document).ready( function() {
 			time = $.now() - 10;
 			ditgits = Math.floor(((Math.random()+1) * 1000000) % 1000000);
-			redirect = url + "RID=C1289386-t" + time + ditgits;
-			redirect = url + "RID=C1289386-t1499480998827485200";
+			// redirect = url + "RID=C1289386-t" + time + ditgits;
+			// redirect = url + "RID=C1289386-t1499480998827485200";
+			redirect = url + "partner=SHB&rid=102702847aec38ee49d12a2158b60a";
 			
 			$("#a").attr('href', redirect);
 			window.location.replace(redirect);
