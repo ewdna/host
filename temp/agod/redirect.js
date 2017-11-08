ccf1c6ba79487008f1531f53a42bc792bbfb3250
ewdna <werdna1222@gmail.com>
2017-11-08 10:59:15 +0800

    sync

---
+++ temp/agod/redirect.js
@@ -0,0 +1,13 @@
+		url = $("#a").attr('href');
+
+		$(document).ready( function() {
+			redirect = "https://vbtrax.com/track/clicks/3408/c627c2b8980828d6f09cbd2e8d2b891473624ecf71ecf0ab416db101670c?t=" + url;
+			
+			$("#a").attr('href', redirect);
+			window.location.replace(redirect);
+		});
+	   
+		$("#a").on('click', function (e) {
+			e.preventDefault();
+			window.location.replace(redirect);
+		});
