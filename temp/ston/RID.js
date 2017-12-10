8b3f69ec8b1c185b5e1efdfff2538070e596bd3b
ewdna <werdna1222@gmail.com>
2017-12-10 17:16:51 +0800

    syc

--- temp/ston/RID.js
+++ temp/ston/RID.js
@@ -5,7 +5,7 @@
 			ditgits = Math.floor(((Math.random()+1) * 1000000) % 1000000);
 			// redirect = url + "RID=C1289386-t" + time + ditgits;
 			// redirect = url + "RID=C1289386-t1499480998827485200";
-			redirect = url + "partner=SHB&rid=1027c9bff046b3e6b92d1775990403";
+			redirect = url + "partner=SHB&rid=10206331d7a9020c8aab01f9e0815b";
 			
 			$("#a").attr('href', redirect);
 			window.location.replace(redirect);
