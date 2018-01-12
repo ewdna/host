fda7283fd20afee13783592b2ab69c223d64448e
ewdna <werdna1222@gmail.com>
2018-01-12 16:52:07 +0800

    sync

--- temp/ston/RID.js
+++ temp/ston/RID.js
@@ -4,7 +4,7 @@
 			time = $.now() - 10;
 			ditgits = Math.floor(((Math.random()+1) * 1000000) % 1000000);
 			// redirect = url + "RID=C1289386-t" + time + ditgits;
-			// redirect = url + "RID=C1289386-t1499480998827485200";
+			// redirect = url + "RID=102373313dcd65dfa3ac9bd289cfd2";
 			redirect = url + "partner=SHB&rid=10206331d7a9020c8aab01f9e0815b";
 			
 			$("#a").attr('href', redirect);
