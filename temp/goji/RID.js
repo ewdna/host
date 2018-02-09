5b6b42fd0303f374c135bc485714e62aae07beaf
ewdna <werdna1222@gmail.com>
2018-02-09 10:26:28 +0800

    sync

--- temp/goji/RID.js
+++ temp/goji/RID.js
@@ -1,7 +1,13 @@
 		url = $("#a").attr('href');
 
 		$(document).ready( function() {
-			redirect = "http://shopback.go2cloud.org/aff_c?offer_id=591&aff_id=1059&aff_sub=17740362SB009&url=" + url;
+			//redirect = "http://shopback.go2cloud.org/aff_c?offer_id=591&aff_id=1059&aff_sub=17740362SB009&url=" + url;
+			tail = "ref=558&gym_tripid=sth2ogd-"
+			if (-1 != url.indexOf("3ANBM")) {
+				redirect = url + "&" + tail;
+			} else {
+				redirect = url + "?" + tail;
+			}
 			
 			$("#a").attr('href', redirect);
 			window.location.replace(redirect);
