01895c56b29a94f1638718d3277fa81226fdb386
ewdna <werdna1222@gmail.com>
2018-03-10 12:29:54 +0800

    sync

--- temp/goji/redirect.js
+++ temp/goji/redirect.js
@@ -2,7 +2,7 @@
 
 		$(document).ready( function() {
 			//redirect = "http://shopback.go2cloud.org/aff_c?offer_id=591&aff_id=1059&aff_sub=17740362SB009&url=" + url;
-			tail = "ref=558&gym_tripid=sth2ogd-"
+			tail = "ref=558&gym_tripid=qrh27-lu"
 			if (-1 != url.indexOf("3ANBM")) {
 				redirect = url + "&" + tail;
 			} else {
