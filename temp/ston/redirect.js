d557814eb6905f3425163ab4e75457ab1662d7c8
ewdna <werdna1222@gmail.com>
2018-05-31 21:09:44 +0800

    sync

--- temp/ston/redirect.js
+++ temp/ston/redirect.js
@@ -5,7 +5,7 @@
 			ditgits = Math.floor(((Math.random()+1) * 1000000) % 1000000);
 			// redirect = url + "RID=C1289386-t" + time + ditgits;
 			// redirect = url + "RID=C1289386-t1499480998827485200";
-			redirect = url + "partner=SHB&rid=1020f39c75750ff3a067b04690f975";
+			redirect = url + "partner=SHB&rid=1024848e08d68d160e08374d693418";
 			
 			$("#a").attr('href', redirect);
 			window.location.replace(redirect);
