a20bba15259d13b551c29b81f8929208f9d313cc
ewdna <werdna1222@gmail.com>
2018-10-05 15:33:25 +0800

    sync

--- temp/ston/redirect.js
+++ temp/ston/redirect.js
@@ -1,11 +1,14 @@
 		url = $("#a").attr('href');
 
 		$(document).ready( function() {
-			time = $.now() - 10;
-			ditgits = Math.floor(((Math.random()+1) * 1000000) % 1000000);
+			// time = $.now() - 10;
+			// ditgits = Math.floor(((Math.random()+1) * 1000000) % 1000000);
 			// redirect = url + "RID=C1289386-t" + time + ditgits;
 			// redirect = url + "RID=C1289386-t1499480998827485200";
-			redirect = url + "partner=SHB&rid=102e29bd74046663245b5376ffce06";
+			
+			redirect = 
+				"http://shopback.go2cloud.org/aff_c?offer_id=1740&aff_id=1059&aff_sub=21986512SB009&url=" + 
+				url + "partner=SHB&rid=102fd92a3075892bb9b58a190ab1d5";
 			
 			$("#a").attr('href', redirect);
 			window.location.replace(redirect);
