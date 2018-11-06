617562ec6774c986ba27decde638c3d32b34d6ed
ewdna <werdna1222@gmail.com>
2018-11-06 20:42:44 +0800

    sync

--- temp/bkin/redirect.js
+++ temp/bkin/redirect.js
@@ -1,9 +1,13 @@
 		url = $("#a").attr('href');
 
 		$(document).ready( function() {
-			// booking 分潤
-			redirect = "https://vbtrax.com/track/clicks/3455/c627c2b8980829daf99cbd2e8d2b891473624ecf71ecf0ab416db1016201?t=" + url;
+			// 聯盟網 booking 分潤
+			/* redirect = "https://vbtrax.com/track/clicks/3455/c627c2b8980829daf99cbd2e8d2b891473624ecf71ecf0ab416db1016201?t=" + url;
+			$("#a").attr('href', redirect);
+			window.location.replace(redirect); */
 			
+			// Booking 官網推薦碼 
+			redirect = url + "?aid=1642106";
 			$("#a").attr('href', redirect);
 			window.location.replace(redirect);
 		});
