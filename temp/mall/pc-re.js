b7aaaab6332c308fb6b2974e1ee2f72f98388b3e
ewdna <werdna1222@gmail.com>
2019-04-04 22:20:33 +0800

    sync

--- temp/mall/pc-re.js
+++ temp/mall/pc-re.js
@@ -1,8 +1,8 @@
 		url = $("#a").attr('href');
 
 		$(document).ready( function() {
-			aff_sub = "26981319SB009";
-			transaction_id = "10231b8827138958f7938ea19f63ed";
+			aff_sub = "27744229SB009";
+			transaction_id = "102314ecf48c3223bccd066340ede0";
 			encodeUrl = encodeURIComponent(url);
 		
 			// https://shopping.pchome.com.tw/platshop/v1/platshop/shopback?utm_source=shopback&utm_medium=cps&transaction_id=10241e5bea4a64b473b85c8ce1424e&rurl=https%3A%2F%2Fshopping.pchome.com.tw%2Fcdn%2Factivity%2Fcampaign%2FC974060247
