f4ab77cc2a21ab762cd170cb3f01e7fff0dc12e3
ewdna <werdna1222@gmail.com>
2019-06-03 21:23:00 +0800

    sync

---
+++ temp/life/17lf-re.js
@@ -0,0 +1,9 @@
+var jquery = document.createElement('script');
+jquery.src = "https://code.jquery.com/jquery-latest.min.js";
+document.getElementsByTagName('head')[0].appendChild(jquery);	
+
+jquery.onload = function(){
+    var script = document.createElement('script');
+	script.src = "../web/conn.redirect.js";
+	document.getElementsByTagName('head')[0].appendChild(script);	
+} 
