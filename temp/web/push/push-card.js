f02f566edc0e99163d7f66f282d8485892457e2b
ewdna <werdna1222@gmail.com>
2019-07-13 22:09:44 +0800

    sync

--- temp/web/push/push-card.js
+++ temp/web/push/push-card.js
@@ -1,10 +1,6 @@
 var push_card_div = $('<div id="push-card-div">');
 push_card_div.append($('<div style="margin:5px; float:left;"><a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-cash.png?env=dev" style="border:1px green dashed;"/></a></div>'));
 push_card_div.append($('<div style="margin:5px; float:left;"><a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-insurance.png?env=dev" style="border:1px green dashed;"/></a></div>'));
-push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-insurance.png?env=dev" style="border:1px green dashed; margin:5px;"/></a>'));
-push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-insurance.png?env=dev" style="border:1px green dashed; margin:5px;"/></a>'));
-push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-insurance.png?env=dev" style="border:1px green dashed; margin:5px;"/></a>'));
-push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-insurance.png?env=dev" style="border:1px green dashed; margin:5px;"/></a>'));
 
 push_card_div.appendTo('#Side-Bar-Ads');
 push_card_div.clone().appendTo('a[name="more"]');
