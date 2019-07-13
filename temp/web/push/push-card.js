50a7442f9b8523588538fa796ac55616e6d1abbb
ewdna <werdna1222@gmail.com>
2019-07-13 21:43:12 +0800

    sync

---
+++ temp/web/push/push-card.js
@@ -0,0 +1,9 @@
+var push_card_div = $('<div id="push-card-div">');
+push_card_div.append($('<div style="margin:5px; float:left;"><a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="./push-card-sinopac-cash.png" style="border:1px green dashed;"/></a></div>'));
+push_card_div.append($('<div style="margin:5px; float:left;"><a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="./push-card-sinopac-insurance.png" style="border:1px green dashed;"/></a></div>'));
+push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="./push-card-sinopac-insurance.png" style="border:1px green dashed; margin:5px;"/></a>'));
+push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="./push-card-sinopac-insurance.png" style="border:1px green dashed; margin:5px;"/></a>'));
+push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="./push-card-sinopac-insurance.png" style="border:1px green dashed; margin:5px;"/></a>'));
+push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="./push-card-sinopac-insurance.png" style="border:1px green dashed; margin:5px;"/></a>'));
+
+push_card_div.appendTo('#Read-More-Ads');
