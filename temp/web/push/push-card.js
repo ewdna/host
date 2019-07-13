9934c820dd9bd623e2fa7cef00160fe0abdf52cf
ewdna <werdna1222@gmail.com>
2019-07-13 22:04:11 +0800

    sync

--- temp/web/push/push-card.js
+++ temp/web/push/push-card.js
@@ -7,4 +7,4 @@ push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" targe
 push_card_div.append($('<a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-insurance.png?env=dev" style="border:1px green dashed; margin:5px;"/></a>'));
 
 push_card_div.appendTo('#Side-Bar-Ads');
-push_card_div.appendTo('a[name="more"]');
+push_card_div.clone().appendTo('a[name="more"]');
