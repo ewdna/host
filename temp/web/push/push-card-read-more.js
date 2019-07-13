dfda5ac2c649afcd30ac24d06bacb277c819f0aa
ewdna <werdna1222@gmail.com>
2019-07-13 22:44:16 +0800

    sync

--- temp/web/push/push-card-read-more.js
+++ temp/web/push/push-card-read-more.js
@@ -1,4 +1,4 @@
-var push_card_div = $('<div id="push-card-read-more">');
+var push_card_div = $('<div id="push-card-read-more" style="loat:left;">');
 push_card_div.append($('<div style="margin:5px; float:left;"><a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-insurance.png?env=dev" style="border:1px green dashed;"/></a></div>'));
 push_card_div.append($('<div style="margin:5px; float:left;"><a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-cash.png?env=dev" style="border:1px green dashed;"/></a></div>'));
 
