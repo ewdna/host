50198938b5058d6af7214376f8335e4e57ad3800
ewdna <werdna1222@gmail.com>
2019-07-13 22:27:45 +0800

    sync

--- temp/web/push/push-card-read-more.js
+++ temp/web/push/push-card-read-more.js
@@ -1,5 +1,5 @@
-var push_card_div = $('<div id="push-card-side-bar">');
+var push_card_div = $('<div id="push-card-read-more">');
 push_card_div.append($('<div style="margin:5px; float:left;"><a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-insurance.png?env=dev" style="border:1px green dashed;"/></a></div>'));
 push_card_div.append($('<div style="margin:5px; float:left;"><a href="https://g.ewdna.com/card/sinopac-fl.html" target="_blank"><img src="https://cdn.staticaly.com/gh/ewdna/host/master/temp/web/push/push-card-sinopac-cash.png?env=dev" style="border:1px green dashed;"/></a></div>'));
 
-push_card_div.appendTo('#Side-Bar-Ads');
+push_card_div.appendTo('a[name="more"]');
