4d46ef3cac15e4cae1bb036fa3d385e64f5193dc
ewdna <werdna1222@gmail.com>
2019-08-03 13:11:30 +0800

    sync

---
+++ temp/web/push/events-list.js
@@ -0,0 +1,61 @@
+var events = `
+<h3>
+網購省錢+賺回饋3步驟</h3>
+網路書城買書、線上購物、旅遊訂房等，只要透過網路消費都可以透過3步驟省錢賺回饋！首先要把握購物網站自身優惠，包含折扣活動/折價券/購物金及各種滿額回饋；從返利網連到購物網再結帳，就享筆筆消費額外回饋；最後再搭配適合的網路購物信用卡刷卡賺回饋！
+<h4>
+步驟零：新用戶推薦優惠別錯過</h4>
+上面快速介紹了網購省錢賺回饋3步驟，但若是第一次註冊或是想辦新的信用卡，記得在註冊/辦卡搭配推薦碼或推薦方案，畢竟新用戶的好康一輩子只有一次，千萬別錯過囉！
+<ul>
+<li><a href="http://www.book-ecoupon.com/2017/09/referral-coupon.html" target="_blank"><b><span style="color: blue;">【相揪賺好康】各類推薦碼/推薦連結大全(持續更新)！</span></b></a></li>
+<li><a href="https://www.ewdna.com/2017/11/referral-card.html" target="_blank"><span style="color: blue;"><b>【相揪賺好康】開戶/信用卡推薦方案優惠大全(持續更新)！</b></span></a></li>
+</ul>
+<h4>
+步驟一：各大網購折扣+折價券/購物金+活動情報</h4>
+這步是要大家把握好各大網購自家優惠，包含折價券/滿額贈/折扣活動等。這些優惠只要消費前關注就能拿好拿滿，<a href="https://www.facebook.com/book.ecoupon" target="_blank">酷碰集散地</a>&nbsp;粉絲專頁天天都有即時情報，有好優惠都會發文提醒大家喔！另外酷碰集散地也持續更新各家免費折價券/購物金，大家可以點到各家購物網的專文整理。
+<ul>
+<li><a href="http://www.book-ecoupon.com/2012/12/helpbuy.html" target="_blank"><b><span style="color: blue;">【折價券大全】酷碰集散地整理各大網購優惠折價券列表</span></b></a></li>
+</ul>
+<h4>
+步驟二：搭配返現網，網路購物筆筆現金回饋 </h4>
+從購物網站本身優惠/折扣/折價券來省錢後，第二步是在消費前記得透過近期新興的各大返現/返利網，只要從返利網連到購物網站，筆筆消費都可以再獲得額外回饋！
+<ul>
+<li><a href="http://www.book-ecoupon.com/2017/10/taiwan-rebate-compare-blog.html" target="_blank"><b><span style="color: blue;">【返利網全攻略】網購筆筆回饋！店家/返利率大比拼！</span></b></a></li>
+</ul>
+<h4>
+步驟三：配合網購信用卡再賺刷卡現金回饋</h4>
+除了折價券+返利網，消費的最後當然還要再從信用卡賺回饋！目前市面上有不少信用卡回饋鎖定網購族，挑對卡在網購付款時可以再多賺個5%！
+<ul>
+<li><a href="https://www.ewdna.com/2018/07/creditcard-online-shopping.html" target="_blank"><b><span style="color: blue;">【信用卡排行榜】最新網路購物現金回饋信用卡推薦！</span></b></a></li>
+</ul>
+
+<h3>
+網購/刷卡優惠攻略一覽</h3>
+<iframe height="1200" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQQ6ZyJiOpkKme5IzTChZ7x-a4AkF7OOU4ZWu0KRI_tFq5OHlMdyRtB2p0oRuDpnOuX0OzgwNO_uzRS/pubhtml?gid=361509848&amp;single=false&amp;widget=true&amp;headers=false" width="640"></iframe>
+
+<h3>
+各大網購/線上書城折價券及活動情報分享</h3>
+(可往下捲動或直接按表單中的頁籤快速切換)</br>
+<iframe height="5500" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQQ6ZyJiOpkKme5IzTChZ7x-a4AkF7OOU4ZWu0KRI_tFq5OHlMdyRtB2p0oRuDpnOuX0OzgwNO_uzRS/pubhtml?gid=0&amp;single=false&amp;widget=true&amp;headers=false" width="640"></iframe>
+
+<h3>
+信用卡推薦申辦專區</h3>
+<ul>
+<li><a href="https://www.ewdna.com/2017/11/referral-card.html" target="_blank"><span style="color: blue;"><b>【相揪賺好康】開戶/信用卡推薦方案優惠大全(持續更新)！</b></span></a></li>
+</ul>
+<iframe height="2500" src="https://docs.google.com/spreadsheets/d/1uZpnh7yvWcviSyPYv-z4kGUufq2Utv3yBlpluYtUMGY/pubhtml?gid=1840752663&amp;single=false&amp;widget=true&amp;headers=false" width="640"></iframe>
+
+<h4>
+國內食衣住行/國外留學遊學必備服務推薦碼優惠</h4>
+<ul>
+<li><a href="http://www.book-ecoupon.com/2017/09/referral-coupon.html" target="_blank"><b><span style="color: blue;">【相揪賺好康】各類推薦碼/推薦連結大全(持續更新)！</span></b></a></li>
+</ul>
+<iframe height="1000" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQQ6ZyJiOpkKme5IzTChZ7x-a4AkF7OOU4ZWu0KRI_tFq5OHlMdyRtB2p0oRuDpnOuX0OzgwNO_uzRS/pubhtml?gid=2051319620&amp;single=true&amp;widget=true&amp;headers=false" width="640"></iframe>`;
+
+var div_id = "#Events-List-Outer"
+
+if ($(div_id).length) {
+	var events_list_div = $('<div id="events_list">');
+	events_list_div.append(events);
+
+	events_list_div.appendTo(div_id);
+}
