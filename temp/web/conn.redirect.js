6caddb8b8a6e8a6bd977519552279b36ad1396a2
ewdna <ewdna@users.noreply.github.com>
2019-09-09 08:53:28 +0800

    sync

--- temp/web/conn.redirect.js
+++ temp/web/conn.redirect.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = "https://code.jquery.com/jquery-latest.min.js";
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(script);	
 
 script.onload = function(){
