d346b1b57608c8a06d2a09480b8e1a29d404d4c7
ewdna <werdna1222@gmail.com>
2019-12-01 09:59:50 +0800

    sync

---
+++ temp/line/line-re.js
@@ -0,0 +1,9 @@
+var jquery = document.createElement('script');
+jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jquery);	
+
+jquery.onload = function(){
+    var script = document.createElement('script');
+	script.src = "../web/conn.redirect.js";
+	document.getElementsByTagName('head')[0].appendChild(script);	
+} 
