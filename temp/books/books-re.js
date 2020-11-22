df7d2ab529f6e504eeabbe9ef0a00eccf24c31de
ewdna <werdna1222@gmail.com>
2020-11-22 16:11:09 +0800

    sync

---
+++ temp/books/books-re.js
@@ -0,0 +1,10 @@
+var jquery = document.createElement('script');
+jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jquery);	
+
+jquery.onload = function(){
+    var script = document.createElement('script');
+	script.src = "../web/conn.redirect.js";
+	// script.src = "../web/pure.redirect.js";
+	document.getElementsByTagName('head')[0].appendChild(script);	
+} 
