8c3e6f688a72bc1b41c2fb1ce8bceae305f2b476
ewdna <ewdna@users.noreply.github.com>
2022-12-07 19:48:24 +0800

    Update mm-point.js

--- temp/mall/mm-point.js
+++ temp/mall/mm-point.js
@@ -4,7 +4,7 @@ url = $("#a").attr('href');
 
 $(document).ready( function() {
 
-	redirect = url + "&memid=6000007978&cid=apuad&oid=1&osm=league"; // 陽
+	redirect = url + "&memid=6000007495&cid=apuad&oid=1&osm=league"; // 陽
 	
 	$("#a").attr('href', redirect);
 	window.location.replace(redirect);
