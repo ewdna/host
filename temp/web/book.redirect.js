5da474afd7544101a88c32145faafdbe7f6ba428
ewdna <ewdna@users.noreply.github.com>
2025-09-08 09:47:33 +0800

    Update book.redirect.js

--- temp/web/book.redirect.js
+++ temp/web/book.redirect.js
@@ -5,15 +5,22 @@
     var originalHref = link.getAttribute("href");
     if (!originalHref) return;
 
-    var isAffiliate = originalHref.includes("/exep/assp.php/books_werdna/");
     var redirectUrl = originalHref;
 
-    if (!isAffiliate) {
-        redirectUrl = originalHref
-            .replace("https://www.books.com.tw/",
-                     "https://www.books.com.tw/exep/assp.php/books_werdna/")
-            .replace("https://activity.books.com.tw/",
-                     "https://www.books.com.tw/exep/assp.php/books_werdna/http_activity/");
+    // 如果不是 books.com.tw 網址，直接轉到指定的本機頁面
+    if (!originalHref.includes("books.com.tw")) {
+        // 這裡會導到 /temp/books/@event.html
+        redirectUrl = "../books/@event.html";
+    } else {
+        // 如果不是 affiliate，就轉換成 affiliate 連結
+        var isAffiliate = originalHref.includes("/exep/assp.php/books_werdna/");
+        if (!isAffiliate) {
+            redirectUrl = originalHref
+                .replace("https://www.books.com.tw/",
+                         "https://www.books.com.tw/exep/assp.php/books_werdna/")
+                .replace("https://activity.books.com.tw/",
+                         "https://www.books.com.tw/exep/assp.php/books_werdna/http_activity/");
+        }
     }
 
     // 更新 href
