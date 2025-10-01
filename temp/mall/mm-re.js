34eb810a356496094e2aa64be3f4712fe643b0a5
ewdna <ewdna@users.noreply.github.com>
2025-10-01 09:34:43 +0800

    Update mm-re.js

--- temp/mall/mm-re.js
+++ temp/mall/mm-re.js
@@ -2,13 +2,11 @@
     function loadRedirectScript(src) {
         var script = document.createElement('script');
         script.src = src;
-        script.async = false;
+        script.onload = function () {
+            console.log(src + " 已載入並執行");
+        };
         document.head.appendChild(script);
     }
 
-    // momo 使用自己的 redirect.js
     loadRedirectScript("mm-point.js");
-
-    // 若要改用其他（例如共用版本），只要改這裡即可
-    // loadRedirectScript("../web/book.redirect.js");
 })();
